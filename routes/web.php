4e9887a5185fae39a5e4419fe9876401820cda6e
siddahantKumar <sidhantlohana@outlook.com>
2021-02-02 16:02:28 +0500

    again chanhes in web.php

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/','Bloodbankcontroller@Admin')->name('Admin');
 
+Route::get('/','Bloodbankcontroller@Admin')->name('Admin');
+
 Route::get('/nav','Bloodbankcontroller@Navbar');
 Route::get('/blood','Bloodbankcontroller@group');
 Route::get('/Donor','Bloodbankcontroller@Donors');
